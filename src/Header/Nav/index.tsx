46916f6177738def4b12d1878d672ff4543201d3
Bjorshol <122232483+Bjorshol@users.noreply.github.com>
2025-12-30 03:44:16 +0100

    Revamp landing page layout

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -7,18 +7,19 @@ import type { Header as HeaderType } from '@/payload-types'
 import { CMSLink } from '@/components/Link'
 import Link from 'next/link'
 import { SearchIcon } from 'lucide-react'
+import { cn } from '@/utilities/ui'
 
-export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
+export const HeaderNav: React.FC<{ data: HeaderType; className?: string }> = ({ data, className }) => {
   const navItems = data?.navItems || []
 
   return (
-    <nav className="flex gap-3 items-center">
+    <nav className={cn('flex items-center gap-5 text-sm font-semibold uppercase tracking-tight', className)}>
       {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="link" />
+        return <CMSLink key={i} {...link} appearance="link" className="text-white hover:text-[#cfe5cf]" />
       })}
-      <Link href="/search">
+      <Link className="text-white hover:text-[#cfe5cf]" href="/search">
         <span className="sr-only">Search</span>
-        <SearchIcon className="w-5 text-primary" />
+        <SearchIcon className="w-5" />
       </Link>
     </nav>
   )
